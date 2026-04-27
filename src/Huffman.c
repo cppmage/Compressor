67bb3809734b80b2254bc6e9e1eb0e91f9d966d7
Alexander <woddow1@gmail.com>
2026-04-27 12:03:12 +0300

    Scalar LZ77 + Haffman

--- src/Huffman.c
+++ src/Huffman.c
@@ -1,9 +1,14 @@
 #include "Huffman.h"
+#include <assert.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "Node.h"
 #include "Node_Heap.h"
+
+#define TRIGGER_MASK ((1ULL)<<63)
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 int compare(const void *a, const void *b) {
     const struct Node *nodeA = (const struct Node *)a;
@@ -22,15 +27,16 @@ uint8_t countBits(uint64_t n) {
     }
     return len;
 }
-void bfs(struct Node* cur, uint64_t code, struct Data* map){
+void bfs(struct Node* cur, uint64_t code, struct Data* map, uint64_t* bits, uint8_t depth){
     if (cur == NULL) return;
     if(cur->left==NULL && cur->right==NULL){
         map[cur->c].code=code;
-        map[cur->c].len = countBits(code);
+        map[cur->c].len = depth;
+        (*bits)+=map[cur->c].len*cur->freq;
         return;
     }
-    bfs(cur->left, code<<1, map);
-    bfs(cur->right, (code<<1)+1, map);
+    bfs(cur->left, code<<1, map, bits, depth+1);
+    bfs(cur->right, (code<<1)+1, map, bits, depth+1);
 }
 
 
@@ -77,12 +83,134 @@ uint64_t huffman_encode(const uint8_t* src, uint64_t size, uint8_t* dst){
         push(&heap, node);
     }
     //building codes
+    uint64_t bits = 0;
     struct Node* root = top(&heap);
-    bfs(root, 0, map);
+    bfs(root, 0, map, &bits, 0);
 
     //encoding
+    int active_fields = 0;
+    for(int i = 0; i<MAX_SYMBOLS; i++){
+        active_fields+=(map[i].len!=0);
+    }
+    dst+=sprintf((char*)dst, "%d %llu ", active_fields, (unsigned long long)bits);
+    for(int i = 0; i<MAX_SYMBOLS; i++){
+        if(map[i].len!=0){
+            dst+=sprintf((char*)dst, "%d %llu %hhu ", i, (unsigned long long)map[i].code, map[i].len);
+        }
+    }
+    uint64_t position_src = 0;
+    uint64_t writen_bites = 0;
+    uint64_t current = 0, awaited = 0;
+    uint8_t accomulated_len = 0, awaited_len = 0;
+    (*dst++)='|';
+    while(1){
+        if(accomulated_len==64){
+            memcpy(dst, &current, sizeof(current));
+            writen_bites+=accomulated_len;
+            dst+=sizeof(current);
+            current = 0;
+            accomulated_len = 0;
+        }
+        else if(accomulated_len<64 && awaited_len!=0){
 
+            uint8_t provide = 64 - accomulated_len;
+            provide = provide<awaited_len ? provide : awaited_len;
+            
+            uint8_t shifts = awaited_len-provide;
 
+            uint64_t request = awaited>>shifts;
+            awaited &= (1ULL << shifts) - 1; 
+
+            awaited_len-=provide;
+
+            current= current<<provide | request;
+            accomulated_len+=provide;
+
+        }
+        else if(awaited_len==0){
+            if(position_src<size){
+                const uint8_t c = src[position_src++];
+                awaited=map[c].code;
+                awaited_len=map[c].len;
+            }
+            else {
+                break;
+            }
+        }
+
+    }
+
+    if(accomulated_len>0){
+            current<<=(64-accomulated_len);
+            memcpy(dst, &current, sizeof(current));
+            writen_bites+=accomulated_len;
+            dst+=sizeof(current);
+            current = 0;
+            accomulated_len = 0;
+    }
+    assert(writen_bites==bits);
+    //*bits_will_be=writen_bites;
+    return writen_bites;
+}
+
+#define MAX_CODE_LEN 12
+uint64_t huffman_decode(const uint8_t* src, uint64_t size, uint8_t* dst){
+    //read header
+    struct Data map[MAX_SYMBOLS];
+    int16_t table[MAX_CODE_LEN][1<<12];
+    memset(table, -1, sizeof(table));
+
+    int active_fields = 0;
+    uint64_t bits = 0;
+    int advance;
+    sscanf((char*)src, "%d %llu%n", &active_fields, (unsigned long long*)&bits, &advance);
+    src+=advance;
+
+    for (int i = 0; i < active_fields; i++) {
+        int symbol;
+        uint64_t code;
+        uint8_t len;
+        
+        sscanf((char*)src, "%d %llu %hhu%n", &symbol, (unsigned long long*)&code, &len, &advance);
+        src += advance;
+        
+        map[symbol].code = code;
+        map[symbol].len = len;
+
+        table[len][code]=symbol;
+    }
+    while(1){
+        if(*src=='|'){
+            src++;
+            break;
+        }
+        src++;
+    }
+    //encoding
+    uint64_t dst_position = 0;
+    uint64_t word = 0, word_len = 0;
+    uint64_t awaited = 0, awaited_len = 0;
+    uint64_t bits_read = 0;
+    while(bits_read<bits){
+        if(awaited_len==0){
+            memcpy(&awaited, src, sizeof(awaited));
+            src+=sizeof(awaited);
+            awaited_len = MIN(64, bits-bits_read);
+            awaited>>=64-awaited_len;
+            bits_read+=awaited_len;
+        }
+        while(awaited_len!=0){
+            word=(word<<1) | (0x1&(awaited>>(awaited_len-1)));
+            word_len++;
+            awaited_len--;
+            int c = table[word_len][word];
+            if(c!=-1){
+                dst[dst_position++]=(uint8_t)c;
+                word = 0;
+                word_len = 0;
+            }
+        }
+    }
+    
     return 0;
-
 }
--- src/main.c
+++ src/main.c
@@ -9,52 +9,21 @@
 
 int main(){
 
-    struct Heap heap = init_heap();
-    struct Node node1, node2, node3, node4;
-    node1.freq=10;
-    node2.freq=20;
-    node3.freq=30;
-    node4.freq=15;
-
-    push(&heap, &node3);
-    printf("%d\n", (int)top(&heap)->freq);
-    push(&heap, &node2);
-    printf("%d\n", (int)top(&heap)->freq);
-    push(&heap, &node1);
-    printf("%d\n", (int)top(&heap)->freq);
-    push(&heap, &node4);
-    printf("%d %d\n", (int)top(&heap)->freq, (int)heap.right_border);
-    pop(&heap);
-    printf("%d %d\n", (int)top(&heap)->freq, (int)heap.right_border);
-    pop(&heap);
-    printf("%d %d\n", (int)top(&heap)->freq, (int)heap.right_border);
-
-    const char* text = "abracadabra hello world hehehehe aaaaaaaaa lalalalalalalalala";
+    const char* text = "abracadabra hello world hehehehe aaaaaaaaa lalalalalalalalala 1212121314919083718378138";
    
-    char mass[1024];
-    char decoded[1024];
-    huffman_encode((uint8_t*)text, strlen(text), (uint8_t*)mass);
-    memset(mass, '\0', sizeof(mass));
-    memset(decoded, '\0', sizeof(decoded));
-    LZ77_ENCODE(text, strlen(text), mass);
+    char LZ77_ENCODED[1024];
+    char HUFFMAN_ENCODED[1024];
+    char HUFFMAN_DECODED[1024];
+    char LZ77_DECODED[1024];
+    memset(LZ77_ENCODED, '\0', sizeof(LZ77_ENCODED));
+    memset(HUFFMAN_ENCODED, '\0', sizeof(HUFFMAN_ENCODED));
+    memset(HUFFMAN_DECODED, '\0', sizeof(HUFFMAN_DECODED));
+    memset(LZ77_DECODED, '\0', sizeof(LZ77_DECODED));
+
+    LZ77_ENCODE(text, strlen(text), LZ77_ENCODED);
+    huffman_encode((uint8_t*)LZ77_ENCODED, strlen(LZ77_ENCODED), (uint8_t*)HUFFMAN_ENCODED);
+    huffman_decode((uint8_t*)HUFFMAN_ENCODED, strlen(HUFFMAN_ENCODED), (uint8_t*)HUFFMAN_DECODED);
+    LZ77_DECODE(HUFFMAN_DECODED, strlen(HUFFMAN_DECODED), LZ77_DECODED);
     
-    LZ77_DECODE(mass, strlen(mass), decoded);
-
-    printf("Encoded: %s\n", mass);
-    printf("Decoded: %s\n", decoded);
-
-    FILE* f = fopen("text.txt", "r");
-
-    if(f==NULL){
-        return 1;
-    }
-    char buffer[255];
-    while(fgets(buffer, 255, f)){
-        printf("%s\n", buffer);
-    }
-    fclose(f);
-
-
-
     return 0;
 }
